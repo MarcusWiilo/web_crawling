3c19ac7195419409dc211a66ce4ace66aaff81ee
MarcusWiilo <marcus@predictbox.io>
2019-03-19 21:11:27 -0300

    att, docs, featura: crawling basketball

---
+++ crawler.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get('https://www.basketball-reference.com/leagues/NBA_2018_totals.html')
+if req.status_code == 200:
+    print('Requisição bem sucedida!')
+    content = req.content
+
+soup = BeautifulSoup(content, 'html.parser')
+
+table = soup.find(name='table')
+
+table_str = str(table)
+df = pd.read_html(table_str)[0]
+
+table = soup.find(name='table', attrs={'id':'confs_standings_W'})
+
+def scrape_stats(base_url, year_start, year_end):
+    years = range(year_start,year_end+1,1)
+
+    final_df = pd.DataFrame()
+
+    for year in years:
+        print('Extraindo ano {}'.format(year))
+        req_url = base_url.format(year)
+        req = requests.get(req_url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        table = soup.find('table', {'id':'totals_stats'})
+        df = pd.read_html(str(table))[0]
+        df['Year'] = year
+        final_df = final_df.append(df)
+    return final_df
+url = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
+df = scrape_stats(url, 2013, 2019)
+
+drop_indexes = df[df['Rk'] == 'Rk'].index # Pega indexes onde a coluna 'Rk' possui valor 'Rk'
+df.drop(drop_indexes, inplace=True) # elimina os valores dos index passados da tabela
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.barplot(x="Year", y="3PA")
